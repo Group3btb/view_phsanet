b993ac873152a511236871793f20190514a39a09
simratha <simratha@acer>
2016-08-06 17:50:38 +0700

    View phsanet

---
+++ src/main/java/phsanet/ViewPhsanetApplication.java
@@ -0,0 +1,12 @@
+package phsanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewPhsanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewPhsanetApplication.class, args);
+	}
+}
