8e4d2cb060d6430aac4f601bfad71c4cea107f99
User <User@User-PC>
2016-08-08 17:03:27 +0700

    script

--- src/main/webapp/resources/views/js/script.js
+++ src/main/webapp/resources/views/js/script.js
@@ -1,9 +1,9 @@
-  $(document).ready(function() {
-      $("html").niceScroll({styler:"fb",cursorcolor:"#428BCA"});
-
-        $(window).scroll(function(){
-          if($(this).scrollTop()>=100){
-          $("#scrolltop").fadeIn(300);      
-        } else{$("#scrolltop").fadeOut(300);}
-        });
-  });
+$(document).ready(function() { 
+     $("html").niceScroll({styler:"fb",cursorcolor:"#428BCA"});
+     $(window).scroll(function(){
+       if($(this).scrollTop()>=100){
+       $("#scrolltop").fadeIn(300);
+    
+     } else{$("#scrolltop").fadeOut(300);}
+     });
+});
