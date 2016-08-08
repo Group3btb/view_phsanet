29030196aa7b0432891eadecb9019d84e1d05654
User <User@User-PC>
2016-08-08 08:48:27 +0700

    Add index.jsp

--- src/main/java/phsanet/controllers/view/ViewController.java
+++ src/main/java/phsanet/controllers/view/ViewController.java
@@ -1,10 +1,13 @@
 package phsanet.controllers.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
+	
+	@RequestMapping(value={"/","/home","/index"})
 	public String index(){
-		
+	return "index";	
 	}
 }
