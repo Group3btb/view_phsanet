0eb56aee7fb9347a197e61f8fb8758c2cbc84ee5
simratha <simratha@192.168.178.180>
2016-08-11 09:54:29 +0700

    Add scrap.js and prom

--- src/main/java/phsanet/controllers/admin/ScrapController.java
+++ src/main/java/phsanet/controllers/admin/ScrapController.java
@@ -1,4 +1,10 @@
 package phsanet.controllers.admin;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,4 +16,22 @@ public class ScrapController {
 		return "admin/scrap_url_managerment";
 	}
 	
+	
+	public void s() throws IOException{
+		Document doc = Jsoup.connect("http://www.kaymu.com.kh/men-polos/").get();
+		Elements elements = doc.select(".product");
+		//System.out.println(elements);
+		
+		for(Element e: elements){
+			String image = e.select("img").attr("data-layzr");
+			String title = e.select(".ellipsis").text();
+			String price = e.select(".price").text();
+			String link = e.select(".card-overlay").attr("href");
+		
+			System.out.println(image);
+			System.out.println(title);
+			System.out.println(price);
+			System.out.println(link + "\n");
+		}
+	}
 }
--- src/main/webapp/resources/admins/javascripts/scrap.js
+++ src/main/webapp/resources/admins/javascripts/scrap.js
@@ -5,25 +5,57 @@
  var app = angular.module("Myapp",[]);
 app.controller("controller_scrap",function($http,$scope){
  		
-		$scope.test = function(){
-			$scope.findall_web_sources();
-		}
-
  		$scope.findall_web_sources = function(){
  			
 	 	$http({
 	 		url:'/sitemanagerment',
 	 		method:'GET'
 	 	}).then(function(respone){
+	 		alert("HEllo");
 	 		$scope.webs = respone.data.DATA;
 	 		console.log($scope.webs);
 	 	},function(respone){
 	 		alert("Error");
 	 	});
-	 	$scope.findall_web_sources();
 	 }// eng method site
+	 $scope.findall_web_sources();
 
 
+	 $scope.findall_subcategory = function(){
+	 	$http({
+	 		url:'/subcategories',
+	 		method:'GET'
+	 	}).then(function(respone){
+	 		$scope.subs = respone.data.DATA;
+	 	},function(respone){
+	 		alert("Error");
+	 	});
+	 }// end method find all subcategory
+	  $scope.findall_subcategory();
 
+
+	  /// method operation add new url and update url
+	  $scope.operation = function(){
+
+	  }// end operation 
+
+	  $scope.getData= function(record){
+	  		if(record.url!=null){
+	  			$scope.btn = "UPDATE";
+	  			$scope.title ="UPDATE";
+	  		}else{
+	  			$scope.btn = "Save";
+	  			$scope.title ="NEW";
+	  		}
+	  }
+	  
+	  //getid subcategory
+	  $scope.webSiteChange = function(record){
+	  	alert(record);
+	  }
+	  
+	  $scope.getWeb_source_ID = function(record){
+	  	alert(record.web.getWeb_source_id);
+	  }
 
  });//end controller
