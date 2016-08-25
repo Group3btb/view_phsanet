119e9e084bf3a36ddb89325d083a7046b37e22ba
User <User@User-PC>
2016-08-25 17:20:50 +0700

    controller savelist

---
+++ src/main/java/phsanet/controllers/admin/SaveListController.java
@@ -0,0 +1,18 @@
+package phsanet.controllers.admin;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import phsanet.entitys.User;
+
+@Controller
+public class SaveListController {
+	@RequestMapping(value={"/savelist"})
+	public String mainPage(@AuthenticationPrincipal User user){
+		System.out.println("USER SESION INFORMATION  ===> " + user);
+		return "/user/savelist";
+	}
+	
+}
